a49e54b64c906ab142083c5d55011ddcc0434794
Muhammad Haseeb <bang526726@gmail.com>
2024-10-28 23:53:04 +0500

    Complete todo app using hive

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +11,109 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _textEditingController = TextEditingController();
+  Box? _todoBox;
+
+  @override
+  void initState() {
+    super.initState();
+    Hive.openBox("todo_box").then((_box) {
+      setState(() {
+        _todoBox = _box;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TO DO'),
+        centerTitle: true,
+        backgroundColor: Colors.yellow,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _displayAlertBox(context);
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    if (_todoBox == null) {
+      return const Center(child: CircularProgressIndicator());
+    } else {
+      return ValueListenableBuilder(
+        valueListenable: _todoBox!.listenable(),
+        builder: (context, box, child) {
+          final todoKeys = box.keys.toList();
+          return SizedBox.expand(
+            child: ListView.builder(
+                itemCount: todoKeys.length,
+                itemBuilder: (context, index) {
+                  Map todo = _todoBox!.get(todoKeys[index]);
+                  return Slidable(
+                    endActionPane:
+                        ActionPane(motion: StretchMotion(), children: [
+                      SlidableAction(
+                        onPressed: (context) {
+                          _todoBox!.delete(todoKeys[index]);
+                        },
+                        backgroundColor: Color(0xFFFE4A49),
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: 'Delete',
+                      ),
+                    ]),
+                    child: ListTile(
+                      title: Text(todo["content"]),
+                      subtitle: Text(todo["createdTime"].toString()),
+                      trailing: Checkbox(
+                          value: todo["isDone"],
+                          onChanged: (value) {
+                            todo["isDone"] = value;
+                            _todoBox!.put(todoKeys[index], todo);
+                          }),
+                    ),
+                  );
+                }),
+          );
+        },
+      );
+    }
+  }
+
+  Future _displayAlertBox(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Enter your Task'),
+          content: TextField(
+            controller: _textEditingController,
+            decoration: const InputDecoration(
+              hintText: "Todo....",
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                _todoBox?.add({
+                  "content": _textEditingController.text,
+                  "createdTime": DateTime.now(),
+                  "isDone": false
+                });
+                Navigator.pop(context);
+                _textEditingController.clear();
+              },
+              child: const Text('Okay'),
+            )
+          ],
+        );
+      },
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
